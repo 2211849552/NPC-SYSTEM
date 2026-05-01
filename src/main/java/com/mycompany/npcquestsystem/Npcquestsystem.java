1f2823167a6ed9d8cda412cb4d2984cd0cb6e3f3
(2211849552)لجين كمال الاسطى <lojainalosta9@gmail.com>
2026-05-01 15:36:46 +0200

    initial setup

---
+++ src/main/java/com/mycompany/npcquestsystem/Npcquestsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.npcquestsystem;
+
+/**
+ *
+ * @author HP
+ */
+public class Npcquestsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
